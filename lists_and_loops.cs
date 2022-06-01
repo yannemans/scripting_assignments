b6b705914c32dd78e73beb060ca0e4c80936492d
yannemans <74655615+yannemans@users.noreply.github.com>
2022-06-01 11:34:36 +0200

    init

---
+++ lists_and_loops.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic; // add this to use lists
+
+namespace Week_1._2_ListsAndLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region topics
+            /*
+             * Lists & Loops
+             * - populating a list with a loop
+             * - loop through a list with a foreach
+             */
+            #endregion
+
+            #region populating a list with a loop
+            // ## POPULATING USING A LOOP
+            // create a list, this one is empty
+            var numberList = new List<int>();
+
+            // create a loop that runs 5 times
+            for (int i = 0; i < 5; i++)
+            {
+                // put the index number in the list
+                numberList.Add(i);
+            }
+            // the list now contains 0, 1, 2, 3, 4
+
+            // create an array with names
+            // and an empty list
+            string[] themNames = new string[] { "summit1g", "Riot Games", "Syndicate", "Shroud", "ESL_CSGO" };
+            var stringList = new List<string>();
+
+            // loop over the items in the array with a foreach
+            foreach (var name in themNames)
+            {
+                // add the item to the list
+                stringList.Add(name);
+            }
+            #endregion
+
+            #region loop through list with a foreach
+            // ## LOOP THROUGH A LIST
+            // create a list, this one is not empty
+            var theThings = new List<string>() { "derp", "dank", "bonk", "stonk"};
+
+            // loop over the items in the list with a foreach
+            foreach (var thing in theThings)
+            {
+                // write the content to the console
+                Console.WriteLine(thing);
+            }
+            #endregion
+        }
+    }
+}
---
+++ random_response.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace Week_2._3_Tooltje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Maak een toolje wat...
+             * - de gebruiker vraagt om het te starten
+             * - een lijst met minimaal 3 voorwerpen, karakters, shows of iets dergelijks doorloopt
+             * - per optie de gebruiker vraagt of deze dit leuk vind ja of nee 
+             *   protip: antwoorden kun je opslaan in een nieuwe array
+             * - per vraag een random antwoord geeft (keuze uit minimaal 6)
+             * - na alle opties gehad te hebben de leuk gevonden opties laat zien
+             *   en een mening over de keuze geeft (keuze uit minimaal 6) 
+             *
+             * Zorg voor voldoende informatie naar de gebruiker
+             * wees creatief, schrijf je eigen code en maak er iets leuks van.
+             */
+
+            /*
+             * Tips
+             * - Gebruik arrys.
+             * - Met loops en ifjes kom je een heel eind...
+             * - Als je door een array loopt met een loop kun je de index
+             *   gebruiken om dezelfde locatie in een andere array te vullen.
+             *   Of om informatie op dezelfde positie in een andere array op te halen.
+             * - Bij de vraag of de gebruiker iets leuk vind of niet kun je foute invoer 
+             *   gewoon als een ja of nee interpreteren. 
+             */
+
+            #region app
+            Random rnd = new Random();
+            string[] objects = { "pink", "monkey", "potato" };
+            string[] responses = new string[6];
+            string[] reviews = new string[8];
+            
+            string user;
+            string userInput;
+            bool[] userLikes = new bool[objects.Length];
+
+            responses[0] = "Oh my...";
+            responses[1] = "You dont say...";
+            responses[2] = "Hmmm... interesting...";
+            responses[3] = "One wonders...";
+            responses[4] = "Hahaha!! Ow, sorry...";
+            responses[5] = "ieuw... scary...";
+
+            reviews[0] = "Lorem ipsum dolor.";
+            reviews[1] = "What? No way...";
+            reviews[2] = "Monkey monkey monkey... banana";
+            reviews[3] = "Bsdesfsd..sdfsdfsewfs!";
+            reviews[4] = "Whoever this is, leave me alone!";
+            reviews[5] = "There are many moons but only one is ours.";
+            reviews[6] = "It sucks...";
+            reviews[7] = "I love it, please give me more...";
+
+            Console.WriteLine("What is you name huuuuman? : ");
+            userInput = Console.ReadLine();
+            user = userInput;
+
+            for (int i = 0; i < objects.Length; i++)
+            {
+                Console.Write($"Do you like {objects[i]}? [y/n]: ");
+                userInput = Console.ReadLine().ToLower();
+
+                if (userInput == "y") 
+                {
+                    userLikes[i] = true;
+                    Console.WriteLine(RandomResponse());
+                }
+                else if (userInput == "n")
+                {
+                    userLikes[i] = false;
+                    Console.WriteLine(RandomResponse());
+                }
+                else
+                {
+                    userLikes[i] = false;
+                    Console.WriteLine("Ok... I'll take that as a no...");
+                }
+
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < userLikes.Length; i++)
+            {
+                if (userLikes[i] == true)
+                {
+                    Console.WriteLine($"{user}, you like {objects[i]}... we say this about {objects[i]} : ");
+                    Console.WriteLine("\"" + RandomReview() + "\"");
+                    Console.WriteLine();
+                }
+            }
+
+            string RandomResponse()
+            {
+                return responses[rnd.Next(0, responses.Length)];
+            }
+
+            string RandomReview()
+            {
+                return reviews[rnd.Next(0, reviews.Length)];
+            }
+
+            #endregion
+        }
+    }
+}
